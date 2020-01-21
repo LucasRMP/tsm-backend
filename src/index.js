ae67a213b45bb11fea5e57403fca2ba02a44cb41
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-21 16:07:16 -0200

    First app controller and route

---
+++ src/index.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  return res.status(200).json({ Hello: 'World' });
+});
+
+app.listen(process.env.PORT || 3000);
